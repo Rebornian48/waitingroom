a8cfd0cb747055ffec6edf7d2524f00abd915de2
Rebornian48 <80138640+Rebornian48@users.noreply.github.com>
2024-01-11 01:26:43 +0700

    update V1

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-var date = new Date();
-document.getElementById("year").innerText = date.getFullYear();
-document.getElementById("last-updated").innerText = date.toLocaleDateString("id-ID") + " " + date.toLocaleTimeString("id-ID");
-
-function countdownTimer() {
-    const difference = +new Date("2024-01-01") - +new Date();
-    let remaining = "Selamat Tahun Baru 2024";
-    if (difference > 0) {
-        const parts = {
-        hari: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        jam: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        menit: Math.floor((difference / 1000 / 60) % 60),
-        detik: Math.floor((difference / 1000) % 60),
-        };
-        remaining = Object.keys(parts).map(part => {
-        return `${parts[part]} ${part}`;  
-        }).join(" ");
-    }
-    document.getElementById("waitTime").innerHTML = remaining;
-}
-countdownTimer();
-
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("change");
 var kirim = document.getElementById("kirim");
