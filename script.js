e346037588039e690879a78d73b1da10772bbe1f
Rebornian48 <80138640+Rebornian48@users.noreply.github.com>
2024-01-14 21:47:26 +0700

    fix bugs

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ var btn = document.getElementById("change");
 var kirim = document.getElementById("kirim");
 
 var date = new Date();
-document.getElementById("last-updated").innerText = date.toLocaleDateString("id-ID") + " " + date.toLocaleTimeString("id-ID");
+document.getElementById("last-updated").innerText =
+  date.toLocaleDateString("id-ID") + " " + date.toLocaleTimeString("id-ID");
 
 btn.onclick = function () {
-    pesan.style.display = 'block';
-    kirim.style.display = 'block';
-    btn.style.display = 'none';
-}
+  pesan.style.display = "block";
+  kirim.style.display = "block";
+  btn.style.display = "none";
+};
 
-kirim.addEventListener('click', () => {
-    var pesan = document.getElementById("pesan");
-    var wait = document.getElementById("waitTime");
-    var newText = pesan.value;
-    wait.innerText = newText;
-    function countdownTimer(){};
-    pesan.style.display = 'none';
-    kirim.style.display = 'none';
-    btn.style.display = 'block';
-})
+kirim.addEventListener("click", () => {
+  var pesan = document.getElementById("pesan");
+  var wait = document.getElementById("waitTime");
+  var newText = pesan.value;
+  wait.innerText = newText;
+  function countdownTimer() {}
+  pesan.style.display = "none";
+  kirim.style.display = "none";
+  btn.style.display = "block";
+});
